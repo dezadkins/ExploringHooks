0ad8b0354b24f261f2062da30a2f47ec41daefa0
River <cosmos.cha1997@gmail.com>
2020-12-09 19:00:56 -0500

    finished!

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,18 +1,29 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import React from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import "./ProductView.css";
 
 function ProductView({ products }) {
-  const [pokemon, setPokemon] = useState(true);
-  const [batman, setBatman] = useState();
+  const [pokemon, setPokemon] = useState(true); //set side open
+  const [batman, setBatman] = useState(); // css thingy
 
+  useEffect(() => {
+    console.log(`selectedProduct CHANGED TO`, batman);
+    if (batman) setPokemon(true);
+  }, [batman]);
+
+  useEffect(() => {
+    console.log(`sideOpen CHANGED TO`, pokemon);
+    if (!pokemon) setBatman();
+  }, [pokemon]);
+
+  console.log(`this is working`);
   return (
     <div className="product-view">
       <div className="product-main-area">
         <h1>Products</h1>
+
         <div className="product-list">
           {products.map((item) => (
             <ProductListItem
@@ -30,7 +41,7 @@ function ProductView({ products }) {
             className="product-side-panel-toggle"
             onClick={() => setPokemon(!pokemon)}
           >
-            {pokemon ? ">" : "<"
+            {pokemon ? ">" : "<"}
           </div>
         </div>
         <ProductDetails visible={pokemon} product={batman} />
